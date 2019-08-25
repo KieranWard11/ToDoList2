345c4582de4c11494a64772835babff80bd6129a
KieranWard11 <kieranward11@gmail.com>
2019-08-25 16:23:27 +0100

    Lab 6, part 2

--- app/src/main/java/org/wit/placemark/activities/PlacemarkListActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkListActivity.kt
@@ -26,8 +26,7 @@ class PlacemarkListActivity : AppCompatActivity(), PlacemarkListener {
         //layout and populate for display
         val layoutManager = LinearLayoutManager(this)
         recyclerView.layoutManager = layoutManager
-        recyclerView.adapter = PlacemarkAdapter(app.placemarks.findAll(), this)
-
+        loadPlacemarks()
 
         //enable action bar and set title
         toolbarMain.title = title
@@ -51,7 +50,16 @@ class PlacemarkListActivity : AppCompatActivity(), PlacemarkListener {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        recyclerView.adapter?.notifyDataSetChanged()
+        loadPlacemarks()
         super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun loadPlacemarks() {
+        showPlacemarks(app.placemarks.findAll())
+    }
+
+    fun showPlacemarks (placemarks: List<PlacemarkModel>) {
+        recyclerView.adapter = PlacemarkAdapter(placemarks, this)
+        recyclerView.adapter?.notifyDataSetChanged()
     }
 }
--- app/src/main/java/org/wit/placemark/main/MainApp.kt
+++ app/src/main/java/org/wit/placemark/main/MainApp.kt
@@ -4,16 +4,19 @@ import android.app.Application
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.wit.placemark.models.PlacemarkMemStore
+import org.wit.placemark.models.PlacemarkStore
 import org.wit.placemark.models.PlacemarkModel
 
 class MainApp: Application(), AnkoLogger {
 
     //val placemarks = ArrayList<PlacemarkModel>()
-    val placemarks = PlacemarkMemStore()
+    //val placemarks = PlacemarkMemStore()
+    lateinit var placemarks: PlacemarkStore
 
     override fun onCreate() {
         super.onCreate()
-        info("Placemark Started")
+        placemarks = PlacemarkMemStore()
+        info("To Do List Started")
         //placemarks.add(PlacemarkModel("One", "About one..."))
         //placemarks.add(PlacemarkModel("Two", "About two..."))
         //placemarks.add(PlacemarkModel("Three", "About three..."))
