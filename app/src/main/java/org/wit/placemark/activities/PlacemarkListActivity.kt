95c84b65afce7311f1000c8d949fdda0593db320
KieranWard11 <kieranward11@gmail.com>
2019-08-27 17:27:49 +0100

    Refactored activity_task_list.xml

--- app/src/main/java/org/wit/placemark/activities/PlacemarkListActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/PlacemarkListActivity.kt
@@ -5,18 +5,14 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.view.*
-import kotlinx.android.synthetic.main.activity_placemark_list.*
-import kotlinx.android.synthetic.main.card_task.*
+import kotlinx.android.synthetic.main.activity_task_list.*
 import org.jetbrains.anko.intentFor
 import org.jetbrains.anko.startActivityForResult
-import org.jetbrains.anko.toast
 import org.wit.placemark.PlacemarkAdapter
 import org.wit.placemark.PlacemarkListener
 import org.wit.placemark.R
 import org.wit.placemark.main.MainApp
 import org.wit.placemark.models.PlacemarkModel
-import java.lang.Thread.sleep
-import java.nio.file.Files.delete
 
 class PlacemarkListActivity : AppCompatActivity(), PlacemarkListener {
 
@@ -24,7 +20,7 @@ class PlacemarkListActivity : AppCompatActivity(), PlacemarkListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_placemark_list)
+        setContentView(R.layout.activity_task_list)
         app = application as MainApp
 
         //layout and populate for display
