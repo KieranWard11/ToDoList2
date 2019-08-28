eadd688ebbc790854adb2d69e51f7974af7c04f2
KieranWard11 <kieranward11@gmail.com>
2019-08-28 10:49:37 +0100

    Improved readability

--- app/src/main/java/org/wit/placemark/activities/TaskActivity.kt
+++ app/src/main/java/org/wit/placemark/activities/TaskActivity.kt
@@ -1,8 +1,8 @@
 package org.wit.placemark.activities
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
@@ -18,7 +18,6 @@ import org.wit.placemark.main.MainApp
 import org.wit.placemark.models.PlacemarkModel
 
 class TaskActivity : AppCompatActivity(), AnkoLogger {
-
     var placemark = PlacemarkModel()
     lateinit var app: MainApp
     var edit = false
@@ -30,15 +29,19 @@ class TaskActivity : AppCompatActivity(), AnkoLogger {
         app = application as MainApp
 
         if (intent.hasExtra("placemark_edit")) {
+
             edit = true
+
             placemark = intent.extras.getParcelable<PlacemarkModel>("placemark_edit")
             placemarkTitle.setText(placemark.title)
             placemarkDescription.setText(placemark.description)
             placemarkPriority.setText(placemark.priority)
             taskImage.setImageBitmap(readImageFromPath(this, placemark.image))
+
             if (placemark.image != null) {
                 chooseImage.setText(R.string.change_task_image)
             }
+
             taskImage.visibility=View.VISIBLE
             btnAdd.setText(R.string.button_saveTask)
         }
@@ -47,9 +50,11 @@ class TaskActivity : AppCompatActivity(), AnkoLogger {
             placemark.title = placemarkTitle.text.toString()
             placemark.description = placemarkDescription.text.toString()
             placemark.priority = placemarkPriority.text.toString()
+
             if (placemark.title.isEmpty()) {
                 toast(R.string.enter_taskTitle)
-            } else {
+            }
+            else {
                 if (edit) {
                     app.placemarks.update(placemark.copy())
                 } else {
@@ -86,15 +91,15 @@ class TaskActivity : AppCompatActivity(), AnkoLogger {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-      super.onActivityResult(requestCode, resultCode, data)
-      when (requestCode) {
-        IMAGE_REQUEST -> {
-          if (data != null) {
-              placemark.image = data.getData().toString()
-              taskImage.setImageBitmap(readImage(this, resultCode, data))
-              taskImage.visibility=View.VISIBLE
-          }
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            IMAGE_REQUEST -> {
+                if (data != null) {
+                    placemark.image = data.getData().toString()
+                    taskImage.setImageBitmap(readImage(this, resultCode, data))
+                    taskImage.visibility=View.VISIBLE
+                }
+            }
         }
-      }
     }
 }
